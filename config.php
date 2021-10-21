940ae3bb07651f20486ddf1e29d217aedcd09198
graver <undergraver75@gmail.com>
2021-10-21 11:52:27 +0300

    config

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'base_host' => 'https://soundcloud.com',
+    'api_host'  => 'https://api-v2.soundcloud.com',
+    'client_id' => '6gsNBd4mJwXr0LxTBh8VKBOrViK6Aj56', // в .env вынести по-хорошему, но мне не хочется ради одного пакета тянуть композер
+
+    'db_host'   => 'localhost',
+    'db_name'   => 'soundcloud',
+    'db_user'   => 'graver',
+    'db_pass'   => 'hesoyam2712',
+
+    'artists'   => [
+        'lakeyinspired', 'dekobe', 'birocratic'
+    ]
+];
